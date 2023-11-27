5b520a1624abd870673dd091a1cb405e7589a8da
galal <galalmohamed25325@gmail.com>
2023-11-27 17:09:19 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/Models/FriendType.java

--- src/Models/User.java
+++ src/Models/User.java
@@ -1,6 +1,15 @@
 package Models;
+import  java.util.*;
+
+import java.util.Date;
 
 public class User {
-    public String name;
-
+    private String email;
+    private String last_name;
+    private String first_name;
+    private boolean martial_status;
+    private String password;
+    private String phone;
+    private Date birth_date;
+//    Map<User, > hm = new HashMap<String, Integer>();
 }
