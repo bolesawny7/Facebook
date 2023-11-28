f9208eb768a16cd47a24bfcbc5c6e3b01d0d3217
ahmeda4f <ahmedashrafroma@gmail.com>
2023-11-28 18:07:44 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/Models/User.java

--- src/Models/Post.java
+++ src/Models/Post.java
@@ -1,22 +1,56 @@
 package Models;
 
-import javax.xml.crypto.Data;
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Date;
+import java.util.ArrayList;
+
 
 public class Post {
     private User createdBy;
     private Date creationDate;
     private String content;
     protected User[] tagged;
-    private boolean privacyoption; //(0 friends , 1 public)
+    private static int postCounter=1;
+    private final int  postId =postCounter;
+    private boolean privacyOption; //(0 friends , 1 public)
     //private React[] reacts;
 
 
-    public Post(User createdBy, Date creationDate, String content, User[] tagged, boolean privacyoption) {
+
+   // post without tag
+    public Post(User createdBy, Date creationDate,  boolean privacyOption , String content)
+    {
         this.createdBy = createdBy;
         this.creationDate = creationDate;
         this.content = content;
-        this.tagged = tagged;
-        this.privacyoption = privacyoption;
+        this.privacyOption = privacyOption;
+        postCounter++;
     }
+
+    // post with tag
+    public Post(User createdBy, Date creationDate, boolean privacyOption , String content, User[] tagged )
+    {
+        this(createdBy,creationDate,privacyOption,content);
+        this.tagged = tagged;
+    }
+
+    public void editPost(String content , boolean privacyOption )
+    {
+        this.content = content;
+        this.privacyOption = privacyOption;
+    }
+    public void editPost(String content )
+    {
+        this.content = content;
+    }
+    public void editPost(boolean privacyOption )
+    {
+        this.privacyOption = privacyOption;
+    }
+
+//    public void react(React reaction)
+//    {
+//        reacts.push
+//    }
 }
