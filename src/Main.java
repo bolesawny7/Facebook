c5af5cf4974e52081bcedf6ec785b511fc7d42c1
polesawny5@gmail.com <polesawny5@gmail.com>
2023-12-02 03:43:00 +0200

    refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,11 @@
-import Enums.Gender;
-import Models.Users.User;
-import Services.UserService;
-
-import java.util.Date;
-import java.util.Locale;
-import java.util.Scanner;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import Services.Menu;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1-login");
-        System.out.println("2-signUP");
-        Scanner input=new Scanner(System.in);
-        int y= input.nextInt();
-        UserService userService = new UserService();
-        switch (y){
-            case 1:
-                userService.login();
-                break;
-            case 2:
-                userService.signUp();
-                break;
-            default:
-                System.out.println("please enter 1 or 2");
-        }
+        Menu menu = new Menu();
+        menu.mainMenu();
     }
 }
 
