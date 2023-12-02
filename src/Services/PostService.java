2886354e64ba96f516adbf7b229b8ad3e7260644
polesawny5@gmail.com <polesawny5@gmail.com>
2023-12-02 03:43:00 +0200

    refactoring

---
+++ src/Services/PostService.java
@@ -0,0 +1,22 @@
+package Services;
+
+import Models.Post;
+
+import java.util.ArrayList;
+
+public class PostService {
+    Menu menu = new Menu();
+
+    private ArrayList<Post> posts = new ArrayList<Post>();
+
+    public void like() {
+
+    }
+    public void comment() {
+        menu.commentsDashboard();
+    }
+    public void share() {
+
+    }
+
+}
