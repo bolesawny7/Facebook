e82b8340be2d7a8f1f2083e404353c939c501145
polesawny5@gmail.com <polesawny5@gmail.com>
2023-12-02 03:08:26 +0200

    refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import Enums.Gender;
-import Services.UserServices;
+import Models.Users.User;
+import Services.UserService;
 
 import java.util.Date;
 import java.util.Locale;
@@ -11,52 +12,21 @@ import java.time.format.DateTimeFormatter;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("1-login");
         System.out.println("2-signUP");
         Scanner input=new Scanner(System.in);
         int y= input.nextInt();
+        UserService userService = new UserService();
         switch (y){
             case 1:
-                loginFunction();
+                userService.login();
                 break;
             case 2:
-                signupFunction();
+                userService.signUp();
                 break;
             default:
                 System.out.println("please enter 1 or 2");
         }
-    }
-    public static void loginFunction(){
-        Scanner userData=new Scanner(System.in);
-        String email =userData.next();
-        String password =userData.next();
-        UserServices loginservice=new UserServices();
-        loginservice.login(email,password);
-    }
-    public static void signupFunction(){
-
-        Scanner userData=new Scanner(System.in);
-        System.out.println("enter your email");
-        String email =userData.next();
-        System.out.println("enter your first name");
-        String first_name =userData.next();
-        System.out.println("enter your last name");
-        String last_name =userData.next();
-        System.out.println("enter your password");
-        String password =userData.next();
-        System.out.println("enter your gender 0 => male, 1 => female");
-        String genderInput = userData.next();
-        Gender gender = Gender.valueOf(genderInput.toLowerCase());
-        System.out.println("enter your birth_date => year, month, day");
-        int year =userData.nextInt();
-        int month =userData.nextInt();
-        int day =userData.nextInt();
-        Date date = new Date(year,month,day);
-
-        UserServices signupService = new UserServices();
-
-        signupService.signUp(email,first_name, last_name, password, gender, date);
     }
 }
 
---
+++ src/Services/UserService.java
@@ -0,0 +1,53 @@
+package Services;
+
+import Enums.Gender;
+import Models.Users.Client;
+import Models.Users.User;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+public class UserService {
+    private String email;
+    private String password;
+    private ArrayList<Client> clients = new java.util.ArrayList<Client>();
+
+    public void login(){
+        Scanner userData=new Scanner(System.in);
+        String email =userData.next();
+        String password =userData.next();
+        boolean isLogin = false;
+        for (Client client : clients ) {
+            if(client.getEmail().equals(email) && client.getPassword().equals(password)){
+                System.out.println("Login successfully");
+                isLogin = true;
+            }
+        }
+        if(!isLogin){
+            System.out.println("Login failed");
+        }
+    }
+
+
+    public void signUp(){
+        Scanner userData=new Scanner(System.in);
+        System.out.println("enter your email");
+        String email =userData.next();
+        System.out.println("enter your first name");
+        String first_name =userData.next();
+        System.out.println("enter your last name");
+        String last_name =userData.next();
+        System.out.println("enter your password");
+        String password =userData.next();
+        System.out.println("enter your gender 0 => male, 1 => female");
+        String genderInput = userData.next();
+        Gender gender = Gender.valueOf(genderInput.toLowerCase());
+        System.out.println("enter your birth_date => year, month, day");
+        int year =userData.nextInt();
+        int month =userData.nextInt();
+        int day =userData.nextInt();
+        Date birthDate = new Date(year,month,day);
+        User user = new Client( email,  last_name,  first_name,  password,  gender,  birthDate);
+    }
+}
