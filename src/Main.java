a50aff062fadc516cb7fe70b63b27849c013a338
Thomas-Sedhom <thomassedhom97@gmail.com>
2023-12-03 21:41:16 +0200

    b7 + glgl + rsc

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import Services.CommentsService;
+import Services.ConversationService;
+import Services.PostService;
+import Services.UserService;
 import Views.Dashboard;
 
 public class Main {
     public static void main(String[] args) {
+        UserService userService = new UserService();
+        PostService postService = new PostService();
+        CommentsService commentService = new CommentsService();
+        ConversationService conversationService = new ConversationService();
+
         Dashboard dashboard = new Dashboard();
-        dashboard.mainMenu();
+        dashboard.mainMenu(commentService,postService,userService);
     }
 }
 
