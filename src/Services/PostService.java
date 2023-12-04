7154621dbc8ad2d14abdc81f9810424e89f0251b
Thomas-Sedhom <thomassedhom97@gmail.com>
2023-12-04 21:09:07 +0200

    timeline

--- src/Services/PostService.java
+++ src/Services/PostService.java
@@ -1,22 +1,52 @@
 package Services;
 
+import Enums.Gender;
+import Enums.ReactType;
 import Models.Post;
+import Models.Users.Client;
 import Models.Users.User;
 import Views.Dashboard;
+import Services.UserService;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 
 public class PostService {
     Dashboard dashboard = new Dashboard();
+    Scanner input = new Scanner(System.in);
 
-    private ArrayList<Post> posts = new ArrayList<Post>();
+    public User currentUser;
 
-    public void like() {
-
+    public void setCurrentUser(User currentUser)
+    {
+        this.currentUser = currentUser;
     }
 
-    public void share(User account, Post post) {
+    public void like(Post post)
+    {
+        System.out.println("choose react => like, love, wow, haha, sad, angry, care");
+        String reactType = input.next();
+        ReactType react = ReactType.valueOf(reactType.toLowerCase());
+        post.addReact(currentUser, react);
+    }
+
+    public void share(User account, Post post)
+    {
         account.sharePost(post);
     }
 
+    static ArrayList<Post> Timeline(ArrayList<Client> clients)
+    {
+         ArrayList<Post> timeline = new ArrayList<>();
+
+        clients.forEach((User user) -> {
+            ArrayList<Post> userPosts = user.getPosts();
+            timeline.addAll(userPosts);
+        });
+        Collections.shuffle(timeline);
+
+        return timeline;
+    }
 }
