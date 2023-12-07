80dcdf4b15e3b1a9a22ad3032117a2fb5a609d75
Thomas-Sedhom <thomassedhom97@gmail.com>
2023-12-08 00:58:33 +0200

    dashboard

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import Models.Post;
 import Models.React;
+import Models.Users.User;
 import Services.CommentsService;
 import Services.ConversationService;
 import Services.PostService;
@@ -9,14 +11,30 @@ public class Main {
     public static void main(String[] args) {
         UserService userService = new UserService();
         PostService postService = new PostService();
-        CommentsService commentService = new CommentsService();
+        CommentsService commentsService = new CommentsService();
         ConversationService conversationService = new ConversationService();
 
         Dashboard dashboard = new Dashboard();
-        dashboard.mainMenu(commentService,postService,userService);
+
+        int ans = 1;
+        while(true){
+            if(ans == 1)
+                ans = dashboard.mainMenu(commentsService,postService,userService);
+
+            else if(ans == 2)
+                ans = dashboard.userDashboard(commentsService, postService, userService, dashboard.currentUser);
+
+            else if(ans == 3)
+                ans = dashboard.postDashboard( commentsService, postService, userService, dashboard.currentUser, dashboard.post);
+
+            else if(ans == 4)
+                ans = dashboard.commentsDashboard(commentsService, postService, userService, dashboard.currentUser, dashboard.post, dashboard.comment);
+
+            else if(ans == 5)
+                ans = dashboard.ConversationsDashboard( commentsService, postService, conversationService, userService, dashboard.currentUser);
+        }
     }
 }
-
 
 
 
