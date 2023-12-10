c00a3084eb4e7a33c0db0a9902ea6d80f417711f
galal <galalmohamed25325@gmail.com>
2023-12-10 02:19:35 +0200

    userfiles

--- src/Models/Users/Client.java
+++ src/Models/Users/Client.java
@@ -4,22 +4,23 @@ import Enums.Gender;
 import Enums.RelationshipStatus;
 import Models.Users.User;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Client extends User {
-    public Client(String email, String last_name, String first_name, String password, RelationshipStatus status, Gender gender, Date birth_date, String phone) {
+    public Client(String email, String last_name, String first_name, String password, RelationshipStatus status, Gender gender, LocalDate birth_date, String phone) {
         super(email, last_name, first_name, password, status, gender, birth_date, phone);
     }
 
-    public Client(String email, String last_name, String first_name, String password, RelationshipStatus status, Gender gender, Date birth_date) {
+    public Client(String email, String last_name, String first_name, String password, RelationshipStatus status, Gender gender, LocalDate birth_date) {
         super(email, last_name, first_name, password, status, gender, birth_date);
     }
 
-    public Client(String email, String last_name, String first_name, String password, String phone, Gender gender, Date birth_date) {
+    public Client(String email, String last_name, String first_name, String password, String phone, Gender gender, LocalDate birth_date) {
         super(email, last_name, first_name, password, phone, gender, birth_date);
     }
 
-    public Client(String email, String last_name, String first_name, String password, Gender gender, Date birth_date) {
+    public Client(String email, String last_name, String first_name, String password, Gender gender, LocalDate birth_date) {
         super(email, last_name, first_name, password, gender, birth_date);
     }
 }
