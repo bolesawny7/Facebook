46e95fd506899455ee27b004b5a6e656841492af
Thomas-Sedhom <thomassedhom97@gmail.com>
2023-12-14 23:43:04 +0200

    interfaces

---
+++ src/Models/Users/Managements/FriendManagement.java
@@ -0,0 +1,15 @@
+package Models.Users.Managements;
+import Enums.FriendType;
+import Models.Users.User;
+import java.util.ArrayList;
+import java.util.Set;
+
+public interface FriendManagement {
+    void acceptRequest(User user, FriendType friendType);
+    ArrayList<User> getSentFriendRequests();
+    void SentFriendRequest(User user);
+    void removeSentFriendRequest(User user);
+    Set<User> getFriends();
+    FriendType getFriendType(User user);
+    ArrayList<User> getMutual(User user);
+}
